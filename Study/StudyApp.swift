54a2768d379e714b23f65f06b9f9ea92cbad3247
b12705010 <153187156+b12705010@users.noreply.github.com>
2024-09-15 00:37:03 +0800

    Initial Commit

---
+++ Study/StudyApp.swift
@@ -0,0 +1,20 @@
+//
+//  StudyApp.swift
+//  Study
+//
+//  Created by 賴楠天 on 2024/9/15.
+//
+
+import SwiftUI
+
+@main
+struct StudyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
